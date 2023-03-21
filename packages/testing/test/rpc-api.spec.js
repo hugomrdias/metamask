da8b17d07cd403a05eb825d6619456c2de07ea2d
Hugo Dias <hugomrdias@gmail.com>
2023-03-21 15:48:19 +0000

    chore: test with one

--- packages/testing/test/rpc-api.spec.js
+++ packages/testing/test/rpc-api.spec.js
@@ -150,7 +150,8 @@ test.describe('filsnap api with default seed', () => {
     expect(result).toBe(0)
   })
 
-  test('should sign raw message', async ({ metamask, page }) => {
+  // eslint-disable-next-line no-only-tests/no-only-tests
+  test.only('should sign raw message', async ({ metamask, page }) => {
     await metamask.invokeSnap({
       snapId: SNAP_ID,
       request: {
