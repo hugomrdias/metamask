7d3ab4205a0c9e0fd224f88bbea55c93b093c7cd
Hugo Dias <hugomrdias@gmail.com>
2023-04-05 16:32:21 +0100

    feat: add adapter tests

--- packages/testing/src/app.jsx
+++ packages/testing/src/app.jsx
@@ -1,21 +1,44 @@
+/* eslint-disable unicorn/no-useless-undefined */
+import { enableFilecoinSnap } from '@chainsafe/filsnap-adapter'
+import { useState } from 'preact/hooks'
+
 export function App() {
+  const [error, setError] = useState(
+    /** @type {Error | undefined} */ (undefined)
+  )
+  const [output, setOutput] = useState(
+    /** @type {any | undefined} */ (undefined)
+  )
   return (
     <main class="App">
       <h1>Filsnap Testing</h1>
       <div class="Grid">
         <div class="Box">
           <span class="Box-text">
-            <code>first</code>
+            <button
+              data-testid="enable-snap"
+              onClick={() => {
+                enableFilecoinSnap({ network: 't' }, 'npm:@chainsafe/filsnap')
+                  .then((snap) => {
+                    setOutput(snap)
+                  })
+                  .catch((error_) => {
+                    setError(error_)
+                  })
+              }}
+            >
+              Enable Snap
+            </button>
           </span>
         </div>
         <div class="Box">
           <span class="Box-text">
-            <code>first</code>
+            <code data-testid="error">{error ? error.message : ''}</code>
           </span>
         </div>
         <div class="Box">
           <span class="Box-text">
-            <code>first</code>
+            <code data-testid="output">{JSON.stringify(output)}</code>
           </span>
         </div>
       </div>
