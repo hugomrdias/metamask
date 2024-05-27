e405043f767d1dcd2c11fca6aafb058942d323ea
Hugo Dias <hugomrdias@gmail.com>
2024-05-27 17:03:55 +0100

    chore: timeout

--- packages/tools/playwright.config.js
+++ packages/tools/playwright.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test/e2e',
-  // timeout: process.env.CI ? 60 * 1000 : 30 * 1000,
-  timeout: 5000,
+  timeout: process.env.CI ? 60 * 1000 : 30 * 1000,
   expect: {
     timeout: 5000,
   },
